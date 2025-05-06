40e9337a07e894177e0dacdcda08c61936fcb4d5
Kalyan3081 <vijaykalyan3081@gmail.com>
2025-05-07 00:07:27 +0530

    added files

--- index.js
+++ index.js
@@ -9,34 +9,35 @@ import GenresRouter from "./routes/GenreRoutes.js";
 import { getSentiment } from "./blog/sentiment.js";
 import path from "path";
 
-
 const app = express();
 
 dotenv.config();
 app.use(cors());
 const corsOption = {
     origin: "https://blog-server-y4md.onrender.com/",
-    credentials: true
-}
+    credentials: true,
+};
 app.use(cors(corsOption));
 app.use(express.json());
 
-mongoDB_connection()
+mongoDB_connection();
 const _dirname = path.resolve();
 
+// Serve static files from the dist folder
+app.use(express.static(path.join(_dirname, "client", "dist")));
 
-app.use('/auth', AuthRoutes)
-app.use('/blog', BlogRoutes)
-app.use('/comment', CommentRouter)
+app.use('/auth', AuthRoutes);
+app.use('/blog', BlogRoutes);
+app.use('/comment', CommentRouter);
 app.use("/genres", GenresRouter);
 
-app.use(express.static(path.join(_dirname, "/client/dist")));
+// Handle all other routes by sending index.html
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
-})
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
 
-getSentiment()
+getSentiment();
